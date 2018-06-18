bdb87bf66845c08866e78782a21fba35e377d761
alex.li <alex.xin.li@hotmail.com>
2018-06-19 09:17:48 +1000

    check-in: demo_sql.js

--- examples/nodejs/demo_sql.js
+++ examples/nodejs/demo_sql.js
@@ -1,6 +1,8 @@
 /*
 MySQL set up
  - https://dev.mysql.com/doc/mysql-apt-repo-quick-guide/en/#apt-repo-fresh-install
+ - sudo service mysql status|stop|start
+ - (cli) mysql -h localhost -u root -p demo_db   (password: mysqlroot)
 
 Nodejs set up
 	- (YOU NEED A MYSQL DRIVER to access the database!)
@@ -24,7 +26,14 @@ conn.connect((err)=>{
 	console.log("sql connection is ready!");
 
 	// var qry = "ALTER TABLE customers ADD COLUMN id INT AUTO_INCREMENT PRIMARY KEY";
-	var inser = "INSERT INTO customers (name, address) VALUES ?";
+	var sel = "SELECT * FROM customers where name like ? or address = ?";
+	var selAll = "SELECT * FROM customers ORDER BY name DESC LIMIT 5";
+	var del = "DELETE FROM customers WHERE address = 'Mountain 21'";
+	var upd = "UPDATE customers SET address=? WHERE address=?";
+	var dropTbl = "DROP TABLE IF EXISTS customers";
+	var crtTbl = "CREATE TABLE IF NOT EXISTS customers (name VARCHAR(255), address VARCHAR(255))";
+	var insTbl = "INSERT INTO customers (name, address) VALUES ?";
+	// var inser = "INSERT INTO customers (name, address) VALUES ?";
 	var records = [
     ['John', 'Highway 71'],
     ['Peter', 'Lowstreet 4'],
@@ -41,9 +50,10 @@ conn.connect((err)=>{
     ['Chuck', 'Main Road 989'],
     ['Viola', 'Sideway 1633']	
 	];	
-	conn.query(inser, [records], (err, result)=>{
+	conn.query(insTbl, [records], (err, result, fields)=>{
 		if (err) throw err;
-		console.log("table:customers record inserted:" + result.affectedRows);
+		// console.log("table:customers record inserted:" + result.affectedRows);
+		// console.log(fields);
 		console.log(result);
 	})	
 });
